deab12f6f47fdae06dba50a3075509bf65fa5958
Bear-Tech <ivanmoore11235@gmail.com>
2015-08-03 15:35:18 +1000

    Added my version of the Temperature Converter.
    This is my version - currently unfinished - of the temperature
    converter.

---
+++ Temperature Converter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { convert(); }
+
+            static void convert() { 
+            string unit;
+            string temp;
+            string f = "F";
+            string c = "C";
+            //bool quitnow = false;
+            //while (quitnow == false);
+            //{
+                Console.WriteLine("Would you like to convert from Fahrenheiht or Celcius? Please enter a 'C' or an 'F'.");
+                unit = Console.ReadLine();
+                unit = unit.ToUpper();
+                if (Equals(unit, c) || Equals(unit, f))
+                {
+                    Console.WriteLine("Please enter a temperature to convert");
+                    temp = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("You have not entered a valid unit of measurement. Press any key to continue.");
+                    temp = Console.ReadLine();
+                    convert();
+                }
+           // }
+        }
+    }
+}
